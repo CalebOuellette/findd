9749512301d2d23a8881c5091ba304d421afa45f
Caleb Ouellette <caleb.ouellette@gmail.com>
2024-04-27 12:38:27 -0700

    setup client.

---
+++ src/app/api/connections/route.ts
@@ -0,0 +1,8 @@
+import { openai } from "@/app/lib/openai";
+
+export const dynamic = "force-dynamic";
+export async function POST() {
+  // load users close to the user
+
+  return Response.json({ message: "Hello, world!" });
+}
--- src/app/api/user/route.ts
+++ src/app/api/user/route.ts
@@ -1,4 +1,15 @@
+import { openai } from "@/app/lib/openai";
+
 export const dynamic = "force-dynamic";
-export async function GET() {
+export async function POST() {
+  // embed the text of the user.
+  //
+  const embeddingResult = await openai.embeddings.create({
+    model: "text-davinci-003",
+    input: "Hello, world!",
+  });
+
   return Response.json({ message: "Hello, world!" });
 }
+
+// User table
